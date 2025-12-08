c22f671197edafeca8fbdae26dfe67273331c69d
HariTeja2208 <hariteja.ramasahayam@gmail.com>
2025-12-08 13:16:35 +0530

    step 6&7

--- src/main/java/com/alpha/RideAxis/Repository/VehicleRepository.java
+++ src/main/java/com/alpha/RideAxis/Repository/VehicleRepository.java
@@ -3,13 +3,14 @@ package com.alpha.RideAxis.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alpha.RideAxis.Entites.Vehicle;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
-	 List<Vehicle> findByAvailableStatus(String availableStatus, String string);
-
+	@Query("SELECT v FROM Vehicle v WHERE v.availableStatus = 'Available' AND v.currentcity = :city")
+	List<Vehicle> findAvailableVehiclesByCity(String city);
 }
 
