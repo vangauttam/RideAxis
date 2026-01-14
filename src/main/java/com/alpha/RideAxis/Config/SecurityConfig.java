ea2e06192bc3d9fa1d9a50baa7755485ce35a301
vanga uttam <vangauttam12@gmail.com>
2026-01-14 19:40:18 +0530

    backend issue sloved

--- src/main/java/com/alpha/RideAxis/Config/SecurityConfig.java
+++ src/main/java/com/alpha/RideAxis/Config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
 
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/**", "/booking/**", "/drivers/**", "/customer/**").permitAll()
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
                         .requestMatchers(HttpMethod.PUT, "/**").authenticated()
